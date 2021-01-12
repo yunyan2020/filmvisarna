3679b0ab9432a6642e41a09a3b338e92a3aedb49
Denise <denisebradley92@hotmail.com>
2021-01-12 17:29:47 +0100

    Set up login.vue component
    with some CSS

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Login />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from "./components/Login.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Login },
+};
 </script>
+
+
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="backdrop">
+    <form @submit.prevent>
+      <div class="container">
+        <label>
+          E-MAIL:
+          <input v-model="email" type="text" required />
+        </label>
+        <label>
+          PASSWORD:
+          <input v-model="password" type="password" required />
+        </label>
+        <button>LOG IN</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.backdrop {
+  background: rgba(0, 0, 0, 0.514);
+  width: 100%;
+  height: 100%;
+}
+.container {
+  background: white;
+  max-width: 35em;
+  font-size: 10px;
+  font-weight: 700;
+  border: solid gray 2px;
+  margin: auto;
+  padding: 2em;
+  position: relative;
+  top: 20em;
+}
+
+input {
+  border: none;
+  border-bottom: solid gray 2px;
+  margin: 2em;
+  width: 80%;
+}
+
+button {
+  cursor: pointer;
+}
+</style>
