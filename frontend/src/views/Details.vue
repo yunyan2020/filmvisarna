5f2e64ee3e05a5a4b4dd3cf704aeaf44cb223e61
yunyan2020 <li_yunyan@hotmail.com>
2021-01-14 15:39:36 +0100

    add some field that want to show on website
    1.add some field that want to show on website
    2. add Movie.json on backend side

--- frontend/src/views/Details.vue
+++ frontend/src/views/Details.vue
@@ -1,7 +1,10 @@
 <template>
-  <h1>Movie Details Page </h1>
-  <h3>Param:{{ id }} </h3>
-
+  <h1>Movie Details Page</h1>
+  <h3>Param:{{ id }}</h3>
+  <p>title:{{ movie.title }}</p>
+  <p>productionCountries:{{ movie.productionCountries }}</p>
+  <p>productionYear:{{ movie.productionYear }}</p>
+  <p>genre:{{ movie.genre }}</p>
 </template>
 
 <script>
@@ -11,11 +14,13 @@ export default {
       //get id from url parameter
       return this.$route.params.id
     }
+  },
+  movie(){
+    return this.$store.state.pokemon.filter(p => p.id == this.id)[0])
   }
 
 }
 </script>
 
 <style scoped>
-
 </style>
