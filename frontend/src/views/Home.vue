0853b902499a1991bafa9b287ce6c8aba7d10906
Denise <denisebradley92@hotmail.com>
2021-01-14 16:32:30 +0100

    Removed show images on Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,19 @@
 <template>
-<div class="rootElement">
-  <h1>Home page ></h1>
-  <div v-for="item of movie" :key="item.id">
-    <router-link :to="'/details/' + item.id">
-    <p>{{ item.title }}</p>
-      <img v-bind:src="'./img/' + item.images" />
-    </router-link>
-  </div>
+  <div class="rootElement">
+    <h1>Home page ></h1>
+    <div v-for="item of movie" :key="item.id">
+      <router-link :to="'/details/' + item.id">
+        <p>{{ item.title }}</p>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: [ 'id' ],
+  props: ["id"],
   data() {
-    return {
-      images: 'image.jpg',
-    };
+    return {};
   },
   computed: {
     movie() {
