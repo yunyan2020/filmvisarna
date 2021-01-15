5e0e666a485f0ace88780f6d33350b2842fba8ef
yunyan2020 <li_yunyan@hotmail.com>
2021-01-15 14:47:06 +0100

    Change Movie class's fields

--- frontend/src/views/Details.vue
+++ frontend/src/views/Details.vue
@@ -1,25 +1,24 @@
 <template>
   <h1>Movie Details Page</h1>
   <h3>Param:{{ id }}</h3>
-  <p>title:{{ movie.title }}</p>
-  <p>productionCountries:{{ movie.productionCountries }}</p>
-  <p>productionYear:{{ movie.productionYear }}</p>
+  <p>Title:{{ movie.title }}</p>
+  <p>Countries:{{ movie.countries }}</p>
+  <p>productionYear:{{ movie.year }}</p>
   <p>genre:{{ movie.genre }}</p>
 </template>
 
 <script>
 export default {
-  computed:{
+  computed: {
     id() {
       //get id from url parameter
-      return this.$route.params.id
-    }
+      return this.$route.params.id;
+    },
   },
-  movie(){
-    return this.$store.state.pokemon.filter(p => p.id == this.id)[0])
-  }
-
-}
+  movie() {
+    return this.$store.state.movie.filter((p) => p.id == this.id)[0];
+  },
+};
 </script>
 
 <style scoped>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,21 +1,20 @@
 <template>
-<div class="rootElement">
-  <h1>Home page ></h1>
-  <div v-for="item of movie" :key="item.id">
-    <router-link :to="'/details/' + item.id">
-    <p>{{ item.title }}</p>
-      <img v-bind:src="'./img/' + item.images" />
-    </router-link>
-  </div>
+  <div class="rootElement">
+    <h1>Home page ></h1>
+    <div v-for="item of movie" :key="item.id">
+      <router-link :to="'/details/' + item.id">
+        <p>{{ item.title }}</p>
+        <img v-bind:src="item.poster" />
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: [ id ],
   data() {
     return {
-      images: 'image.jpg',
+      imageUrl: "",
     };
   },
   computed: {
