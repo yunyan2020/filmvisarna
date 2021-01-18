931e4566d562489c5847b2e8044369cc18efdc54
Denise <denisebradley92@hotmail.com>
2021-01-18 10:37:57 +0100

    Small fix endtag routerlink

--- frontend/src/components/homepagecomp/Movies.vue
+++ frontend/src/components/homepagecomp/Movies.vue
@@ -1,6 +1,4 @@
 <template>
-
-
   <div class="container">
     <div class="component-title">
       <h1>Just nu</h1>
@@ -9,7 +7,8 @@
     <div class="movie-list">
       <div v-for="movie in movies" :key="movie.id" class="movie">
         <router-link :to="'/movieshow/details/' + movie.id">
-        <img :src="movie.poster" alt="">
+          <img :src="movie.poster" alt="" />
+        </router-link>
         <div class="movie-details">
           <h6 class="title">
             {{ movie.title }} | {{ movie.duration }} | {{ movie.rated }}
@@ -21,72 +20,71 @@
 </template>
 
 <script>
-
 export default {
   props: ["id"],
   computed: {
     movies() {
       return this.$store.state.movie;
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-  .container {
-    margin: 10px;
-    padding: 1em;
-    border: 1px solid rgba(0, 0, 0, 0.521);
-  }
+.container {
+  margin: 10px;
+  padding: 1em;
+  border: 1px solid rgba(0, 0, 0, 0.521);
+}
 
-  .movie-list {
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-  }
+.movie-list {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+}
 
-  .movie {
-    border-radius: 8px;
-  }
+.movie {
+  border-radius: 8px;
+}
 
-  .movie:hover {
-    opacity: .9;
-  }
+.movie:hover {
+  opacity: 0.9;
+}
 
-  img {
-    border-top-left-radius: 8px;
-    border-top-right-radius: 8px;
-    max-height: 400px;
-  }
+img {
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  max-height: 400px;
+}
 
-  .movie-details {
-    background: #333;
-    position: relative;
-    bottom: 32px;
-    /* padding-left: 5px; */
-    padding: 0 5px 0 5px;
-    height: 20px;
-    border-bottom-left-radius: 8px;
-    border-bottom-right-radius: 8px;
-    /* text-align: center; */
-  }
+.movie-details {
+  background: #333;
+  position: relative;
+  bottom: 32px;
+  /* padding-left: 5px; */
+  padding: 0 5px 0 5px;
+  height: 20px;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  /* text-align: center; */
+}
 
-  .component-title {
-    display: flex;
-    justify-content: space-between;
-    color: black;
-    position: relative;
-    top: 25px;
-    padding: 10px;
-  } 
+.component-title {
+  display: flex;
+  justify-content: space-between;
+  color: black;
+  position: relative;
+  top: 25px;
+  padding: 10px;
+}
 
-  .component-title p {
-    position: relative;
-    top: 35px;
-    right: 10px;
-  }
+.component-title p {
+  position: relative;
+  top: 35px;
+  right: 10px;
+}
 
-  .component-title p:hover {
-    opacity: .5;
-  }
+.component-title p:hover {
+  opacity: 0.5;
+}
 </style>
--- frontend/src/components/homepagecomp/TopMovies.vue
+++ frontend/src/components/homepagecomp/TopMovies.vue
@@ -8,71 +8,72 @@
       <div v-for="movie in movies" :key="movie.id" class="movie">
         <router-link :to="'/movieshow/details/' + movie.id">
         <img :src="movie.poster" alt="">
+        </router-link>
         <div class="movie-details">
           <h6 class="title">
             {{ movie.title }} | {{ movie.duration }} | {{ movie.rated }}
           </h6>
         </div>
-      </div>
+        </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   props: ["id"],
   computed: {
     movies() {
-      return this.$store.state.movie.filter(movie => movie.score > 7)
-    }
-  }
-}
+      return this.$store.state.movie.filter((movie) => movie.score > 7);
+    },
+  },
+};
 </script>
 
 <style scoped>
-  h1, p {
-    color: black;
-  }
+h1,
+p {
+  color: black;
+}
 
-  .container {
-    margin: 10px;
-    padding: 1em;
-    border: 1px solid rgba(0, 0, 0, 0.521);
-  }
+.container {
+  margin: 10px;
+  padding: 1em;
+  border: 1px solid rgba(0, 0, 0, 0.521);
+}
 
-  .component-title {
-    display: flex;
-    justify-content: space-between;
-  }
+.component-title {
+  display: flex;
+  justify-content: space-between;
+}
 
-  .topmovies-list {
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-  }
+.topmovies-list {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+}
 
-  .movie {
-    border-radius: 8px;
-  }
+.movie {
+  border-radius: 8px;
+}
 
-  .movie:hover {
-    opacity: .9;
-  }
+.movie:hover {
+  opacity: 0.9;
+}
 
-  img {
-    border-top-left-radius: 8px;
-    border-top-right-radius: 8px;
-    max-height: 400px;
-  }
+img {
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  max-height: 400px;
+}
 
-  .movie-details {
-    background: #333;
-    position: relative;
-    bottom: 32px;
-    padding: 0 5px 0 5px;
-    height: 20px;
-    border-bottom-left-radius: 8px;
-    border-bottom-right-radius: 8px;
-  }
+.movie-details {
+  background: #333;
+  position: relative;
+  bottom: 32px;
+  padding: 0 5px 0 5px;
+  height: 20px;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+}
 </style>
