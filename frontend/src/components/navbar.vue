62e68655e9884eca09ed4298d1573bfbffe7f2d8
Tobias Ostergren <ostergrentobias@gmail.com>
2021-01-18 13:02:08 +0100

    added logo
    logo top left

--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -1,13 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/films">Filmer</router-link>
-    <button @click="toggleLogin">Login</button>
-    <div v-if="showLogin" class="login">
-      <Login @close="toggleLogin"> </Login>
+  <wrapper>
+    <div class="logoBox">
+      <img class="logo" src="../assets/logo2.png" />
     </div>
-    <div v-if="showMember">Member pages dropdown..</div>
-  </nav>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/films">Filmer</router-link>
+      <button @click="toggleLogin">Login</button>
+      <div v-if="showLogin" class="login">
+        <Login @close="toggleLogin"> </Login>
+      </div>
+      <div v-if="showMember">Member pages dropdown..</div>
+    </nav>
+  </wrapper>
 </template>
 
 <script>
@@ -37,10 +42,11 @@ a {
   padding: 20px;
   border: 1px;
   font-size: 20px;
+  color: rgb(124, 124, 114);
 }
 
 a:hover {
-  color: rgba(27, 45, 201, 0.281);
+  color: rgba(121, 122, 131, 0.281);
 }
 
 .login {
@@ -51,11 +57,20 @@ a:hover {
 }
 
 nav {
-  margin-top: 5px;
+  padding: 35px;
   height: 30px;
   text-align: center;
   border-bottom: 2px solid rgb(99, 96, 96);
   box-shadow: 1px 3px 3px grey;
+  background-color: rgb(54, 41, 41);
 }
-
+.logoBox {
+  float: left;
+  position: absolute;
+}
+.logo {
+  width: 50%;
+  height: auto;
+  padding-bottom: 20px;
+}
 </style>
