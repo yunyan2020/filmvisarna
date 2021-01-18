665015b36514b08a90f2039c7279780dd28081e3
yunyan2020 <li_yunyan@hotmail.com>
2021-01-18 11:22:58 +0100

    add link of top movie to the detail
    Link top movie to the detail

--- frontend/src/components/homepagecomp/TopMovies.vue
+++ frontend/src/components/homepagecomp/TopMovies.vue
@@ -5,7 +5,8 @@
       <p>mer...</p>
     </div>
     <div class="topmovies-list">
-      <div v-for="movie in topmovies" :key="movie" class="movie">
+      <div v-for="movie in topmovies" :key="movie.id" class="movie">        
+        <router-link :to="'/movieshow/details/' + movie.id">
         <img :src="movie.Poster" alt="">
         <div class="movie-details">
           <h6 class="title">
@@ -18,60 +19,62 @@
 </template>
 
 <script>
-import movies from '../../../movies.json'
+import movies from "../../../movies.json";
 
 export default {
+  props: ["id"],
   data() {
     return {
-      topmovies: movies.filter(movie => movie.Score > 7)
-    }
-  }
-}
+      topmovies: movies.filter((movie) => movie.Score > 7),
+    };
+  },
+};
 </script>
 
 <style scoped>
-  h1, p {
-    color: black;
-  }
+h1,
+p {
+  color: black;
+}
 
-  .container {
-    margin: 10px;
-    padding: 1em;
-    border: 1px solid rgba(0, 0, 0, 0.521);
-  }
+.container {
+  margin: 10px;
+  padding: 1em;
+  border: 1px solid rgba(0, 0, 0, 0.521);
+}
 
-  .component-title {
-    display: flex;
-    justify-content: space-between;
-  }
+.component-title {
+  display: flex;
+  justify-content: space-between;
+}
 
-  .topmovies-list {
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-  }
+.topmovies-list {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+}
 
-  .movie {
-    border-radius: 8px;
-  }
+.movie {
+  border-radius: 8px;
+}
 
-  .movie:hover {
-    opacity: .9;
-  }
+.movie:hover {
+  opacity: 0.9;
+}
 
-  img {
-    border-top-left-radius: 8px;
-    border-top-right-radius: 8px;
-    max-height: 400px;
-  }
+img {
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  max-height: 400px;
+}
 
-  .movie-details {
-    background: #333;
-    position: relative;
-    bottom: 32px;
-    padding: 0 5px 0 5px;
-    height: 20px;
-    border-bottom-left-radius: 8px;
-    border-bottom-right-radius: 8px;
-  }
+.movie-details {
+  background: #333;
+  position: relative;
+  bottom: 32px;
+  padding: 0 5px 0 5px;
+  height: 20px;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+}
 </style>
