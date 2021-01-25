673eda4d55a5ba9c7131d0ad191b80c4b11eb879
yunyan2020 <li_yunyan@hotmail.com>
2021-01-25 17:21:22 +0100

    update from feature-boking

--- frontend/src/views/Details.vue
+++ frontend/src/views/Details.vue
@@ -1,5 +1,5 @@
 <template> 
-  <!-- <div class="movie-head">
+  <div class="movie-head">
     <h1 class="title">{{ movie.title }}</h1>   
     <h3 class = "genre">{{ movie.genre }}</h3> 
     <h4 class="subtitle"> 
@@ -59,57 +59,12 @@
         </a>  
       </div>
     </div> 
-  </div>  --> 
-  <div class="container">
-    <div class="scene-image">
-      <img :src="movie.movieScene" alt="" class="scene">
-    </div>
-    <div class="movie">
-      <div class="poster">
-        <img :src="movie.poster" alt="" class="movie-poster">
-      </div>
-      <div class="movie-information">
-        <h1>{{ movie.title }}</h1>
-        <h4>{{ movie.genre }}</h4>
-        <h6>{{ movie.runtime }} | {{ movie.rated }}</h6>
-      </div>
-    </div>
-    <div class="movie-detail">
-      <h3>{{ movie.plot }}</h3>
-      <div class="information">
-        <h5>Regi: </h5>
-        <h3> {{ movie.director }}</h3>
-        <h5>Skådespelare: </h5>
-        <h3>{{ movie.actors.join(' - ') }}</h3>
-        <h5>Språk: </h5>
-        <h3>{{ movie.language }}</h3>
-        <h5>Premiär: </h5>
-        <h3>{{ movie.year }}</h3>
-      </div>  
-    </div>
-    <div class="dates-list">
-      <!-- <div class="header">
-        <div class="back">
-          <i class="fas fa-chevron-left"></i>
-        </div>
-        <h1>25/1</h1>
-        <div class="forward">
-          <i class="fas fa-chevron-right"></i>
-        </div>
-      </div> -->
-    </div>
-  </div>
+  </div>   
 </template>
 
 
 <script>
 export default {
-  data() {
-    return {
-      times: [],
-      today: ""
-    }
-  },
   computed: {
     id() {
       //get id from url parameter
@@ -119,36 +74,14 @@ export default {
       return this.$store.state.movie.filter((p) => p.id == this.id)[0];
     },
     viewings() {
-      return this.$store.state.allViewings.filter((viewing) => viewing.movie === this.movie.title)
+      return this.$store.state.allViewings;
     },
-    viewingDates() {
-      // Sorts the movie viewing dates from earliest to latest
-      let tempDates = this.viewings.map( viewing => viewing.date.slice(5,10).replace(/\//g, "")).sort((a,b) => a-b)
-      return tempDates
-    }
-  },
-  methods: {
-    setDates() {
-      // Setting the dates for today and two days ahead
-      let tday = new Date();
-      let tmorrow = new Date();
-      tmorrow.setDate(tday.getDate() + 1);
-      let afterTmorrow = new Date();
-      afterTmorrow.setDate(tmorrow.getDate() + 1);
-
-      this.today = tday.toJSON().slice(0, 10).replace(/-/g, "/");
-      this.tomorrow = tmorrow.toJSON().slice(0, 10).replace(/-/g, "/");
-      this.afterTomorrow = afterTmorrow
-        .toJSON()
-        .slice(0, 10)
-        .replace(/-/g, "/");
-    }
-  },
+  }
 }
 </script>
 
 <style scoped>
-/* .movie-head {
+.movie-head {
   margin: 5px;
   padding: 1em;
   max-width: 50em;
@@ -273,67 +206,5 @@ export default {
 img {
   max-width: 400px;
   max-height: 400px;
-} */
-.container {
-  background: #0f0f0f;
-  color: white;
-  margin: 0;
-}
-
-.scene-image {
-  width: 100%;
-  height: 700px;
-  border: 1px solid black;
-}
-
-.scene {
-  width: 100vw;
-  height: 100%;
-  position: relative;
-  bottom: 35px;
-  
-}
-
-.movie {
-  width: 50%;
-  height: 500px;
-  border: 1px solid red;
-  margin: 0 auto;
-  position: relative;
-  bottom: 200px;
-  display: flex;
-}
-
-.movie-poster {
-  border-radius: 8px;
-}
-
-.movie-information {
-  margin-left: 10px;
-  position: relative;
-  top: 200px;
-}
-
-.movie-detail {
-  width: 50%;
-  height: 450px;
-  border: 1px solid green;
-  margin: 0 auto;
-  position: relative;
-  bottom: 150px;
-}
-
-h5 {
-  opacity: 0.6;
-}
-
-.information {
-  margin-top: 40px;
-}
-
-.dates-list {
-  width: 100%;
-  height: 400px;
-  border: 1px solid orange;
 }
 </style>
