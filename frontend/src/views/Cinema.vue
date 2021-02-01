3b441de3a1a4feaf79558de590abb677b112d00b
EdvinArnryd <edvin.arn@hotmail.com>
2021-02-01 23:06:50 +0100

    Updated cinema page
    Changed the styling a little and made it look better. Removed code that was not used.

--- frontend/src/views/Cinema.vue
+++ frontend/src/views/Cinema.vue
@@ -4,8 +4,6 @@
     <div class="secondDiv">
       <Salon />
     </div>
-
-    <div class="seats"></div>
   </div>
 </template>
 
@@ -49,52 +47,5 @@ h1 {
   top: 60%;
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
-  /*margin: auto;
-  width: 50%;
-  border-radius: 5px;*/
-}
-
-.scene {
-  display: flex;
-  justify-content: center;
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-  background: rgb(173, 173, 173);
-  padding: 5px;
-}
-
-.seat {
-  width: 15px;
-  height: 20px;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  background: #eee;
-  border: 1px solid black;
-  margin: 5px;
-  padding: 10px;
-}
-
-.seat:hover {
-  background: rgba(51, 51, 51, 0.534);
-}
-
-/* .click {
-  background: rgba(0, 128, 0, 0.767);
-} */
-
-.display {
-  border: 1px solid black;
-  position: relative;
-  bottom: 20px;
-  margin: 5em;
-  width: 50%;
-  height: 50px;
-  display: flex;
-  justify-content: center;
-  box-shadow: 0 5px 5px #333;
-  /* clip-path: polygon(0 0, 100% 0%, 91% 100%, 10% 100%); */
 }
 </style>
