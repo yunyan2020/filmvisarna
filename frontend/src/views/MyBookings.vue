0e618cbf8fbb43f3c854b358740bb12ad7d46817
Denise <denisebradley92@hotmail.com>
2021-02-02 09:55:40 +0100

    Small div fix

--- frontend/src/views/MyBookings.vue
+++ frontend/src/views/MyBookings.vue
@@ -4,33 +4,30 @@
       <h3>Mina bokningar</h3>
       <div v-if="myBookings">
         <div v-for="booking in myBookings" :key="booking.id" class="booking">
-          <h6>
-           Price: {{ booking.price }}
-            Seats: {{ booking.nrOfSeats }}
-          </h6>
+          <h6>Price: {{ booking.price }} Seats: {{ booking.nrOfSeats }}</h6>
         </div>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    /*  LÄGG TILL I TEMPLATE ISTÄLLET FÖR booking.price
+  /*  LÄGG TILL I TEMPLATE ISTÄLLET FÖR booking.price
              {{ booking.viewing.movie }} | {{ booking.viewing.date }} |
             {{ booking.viewing.time }} | {{ booking.viewing.screen.name }} */
   props: ["id"],
   computed: {
     myBookings() {
-      return this.$store.state.myBookings
+      return this.$store.state.myBookings;
     },
   },
   created() {
-    this.$store.dispatch("fetchMyBookings")
-  }
-}
+    this.$store.dispatch("fetchMyBookings");
+  },
+};
 </script >
 
 <style scoped>
-
 </style>
 
