612668c3b00566a448685aaf22076b068d665351
Denise <denisebradley92@hotmail.com>
2021-02-03 21:52:39 +0100

    Cleaned up last merge

--- frontend/src/views/tempContact.vue
+++ frontend/src/views/tempContact.vue
@@ -1,21 +1,5 @@
 <template>
   <div class="content">
-    <h1 class="title">Kontakta oss</h1>
-    <div class="textContent">
-      <p>
-        Kundtjänst via email: support@filmvisarna.se eller via
-        kontaktformuläret.
-      </p>
-      <br />
-
-      <p>
-        Filmvisarna <br />
-        Box 829<br />
-        211 20 Malmö<br />
-        Sverige<br />
-      </p>
-      <template>
-  <div class="content">
     <div class="textContent">
       <p>
 
