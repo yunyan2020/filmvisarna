b6f0f3ef8538b262b9fa88ffd90093cbb80ca67e
HananAlAni <72067261+HananAlAni@users.noreply.github.com>
2021-02-03 21:27:50 +0100

    Update tempContact.vue

--- frontend/src/views/tempContact.vue
+++ frontend/src/views/tempContact.vue
@@ -12,9 +12,33 @@
         Filmvisarna <br />
         Box 829<br />
         211 20 Malmö<br />
-        Sverige<br />https://css-tricks.com/css-only-carousel/
+        Sverige<br />
+      </p>
+      <template>
+  <div class="content">
+    <div class="textContent">
+      <p>
+
+   
+   
+      </p>
+
+      <p>
+
+      
+       Kundtjänst via email: support@filmvisarna.se eller via
+        kontaktformuläret<br/>
+        Filmvisarna <br />
+        Box 829<br />
+        211 20 Malmö<br />
+        Sverige<br>
         Org.nr: 42051-5555
       </p>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9012.884120772356!2d13.001895193864502!3d55.61556963826834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4653a3f96deef23f%3A0xb01907810704e53!2s211%2020%20Malm%C3%B6!5e0!3m2!1ssv!2sse!4v1612348153726!5m2!1ssv!2sse" 
+      width="400" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    
+
+}
       <p><b> Vi som har jobbat med vår projekt är:</b></p>
       <p> Denise,Yunyan,Hanan,Ermin,Edvin och Tobias:)</p>
     </div>
